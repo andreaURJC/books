99ab9451c987f6e32c5674d423410f0abd249a51
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-12-03 19:06:36 +0100

    Added book attributes

--- src/main/java/com/urjc/books/models/Book.java
+++ src/main/java/com/urjc/books/models/Book.java
@@ -2,6 +2,14 @@ package com.urjc.books.models;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Book {
+    private String title;
+    private String summary;
+    private String author;
+    private int postYear;
+    private List<Comment> comments;
+
 }
