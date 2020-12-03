5f1bd591a9a138fdd67b456019c07c48443ce82d
Juanma <jmgs15@github.com>
2020-12-03 20:00:41 +0100

    CommentRepository implemented

--- src/main/java/com/urjc/books/models/Comment.java
+++ src/main/java/com/urjc/books/models/Comment.java
@@ -1,11 +1,22 @@
 package com.urjc.books.models;
 
-import lombok.Data;
+import lombok.*;
 
-@Data
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
+    private int id;
     private String author;
     private String text;
     private int score;
+    private int bookId;
+
+    public boolean isSameId(int id) {
+        return this.id == id;
+    }
 }
