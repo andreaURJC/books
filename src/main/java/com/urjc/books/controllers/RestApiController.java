f9cc0eb514ad419d0a78b04504a673e76aa18c58
Juanma <jmgs15@github.com>
2020-12-04 17:45:04 +0100

    GetBookById endpoint added.

---
+++ src/main/java/com/urjc/books/controllers/RestApiController.java
@@ -0,0 +1,30 @@
+package com.urjc.books.controllers;
+
+import com.urjc.books.models.Book;
+import com.urjc.books.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestApiController {
+
+    private BookService bookService;
+
+    public RestApiController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return this.bookService.findAll();
+    }
+
+    @GetMapping("/book/{bookId}")
+    public Book getBook(@PathVariable Integer bookId) {
+        return this.bookService.findById(bookId);
+    }
+}
