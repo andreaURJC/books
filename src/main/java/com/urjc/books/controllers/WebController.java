e57e187ca10ccff17d2a53b5b0045600bc9846d0
Juanma <jmgs15@github.com>
2020-12-05 09:15:12 +0100

    Book html added.

--- src/main/java/com/urjc/books/controllers/WebController.java
+++ src/main/java/com/urjc/books/controllers/WebController.java
@@ -1,6 +1,7 @@
 package com.urjc.books.controllers;
 
 import com.urjc.books.models.Book;
+import com.urjc.books.models.Comment;
 import com.urjc.books.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -21,5 +22,17 @@ public class WebController {
         List<Book> books = this.bookService.findAll();
         model.addAttribute("books", books);
         return "home_page_template";
+    }
+
+    @GetMapping("/books/{bookId}")
+    public String getBook(Model model, Integer bookId) {
+        Book book = this.bookService.findById(bookId);
+        model.addAttribute("id", book.getId());
+        model.addAttribute("title", book.getTitle());
+        model.addAttribute("summary", book.getSummary());
+        model.addAttribute("author", book.getAuthor());
+        model.addAttribute("postYear", book.getPostYear());
+        model.addAttribute("comments", book.getComments());
+        return "book";
     }
 }
