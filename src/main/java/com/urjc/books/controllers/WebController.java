5dee53995d747cc99ca42882a23397c14fc19e46
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-12-05 08:30:03 +0100

    Added index.html

--- src/main/java/com/urjc/books/controllers/WebController.java
+++ src/main/java/com/urjc/books/controllers/WebController.java
@@ -4,6 +4,7 @@ import com.urjc.books.models.Book;
 import com.urjc.books.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -14,11 +15,11 @@ public class WebController {
     @Autowired
     private BookService bookService;
 
-    //TODO: review endpoint with JUANMA
     @GetMapping("/home")
     @ResponseBody
-    public String getBooks() {
+    public String getBooks(Model model) {
         List<Book> books = this.bookService.findAll();
+        model.addAttribute("books", books);
         return "home_page_template";
     }
 }
