d7da55567b5dca26f360615ae0505a017df1c7cc
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-12-05 09:35:08 +0100

    Add home page

--- src/main/java/com/urjc/books/controllers/WebController.java
+++ src/main/java/com/urjc/books/controllers/WebController.java
@@ -1,14 +1,10 @@
 package com.urjc.books.controllers;
 
-import com.urjc.books.models.Book;
 import com.urjc.books.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class WebController {
@@ -16,10 +12,8 @@ public class WebController {
     private BookService bookService;
 
     @GetMapping("/home")
-    @ResponseBody
     public String getBooks(Model model) {
-        List<Book> books = this.bookService.findAll();
-        model.addAttribute("books", books);
-        return "home_page_template";
+        model.addAttribute("books", this.bookService.findAll());
+        return "index";
     }
 }
